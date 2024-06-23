841278cdc8da7f4f2b1c2a2ea4ea7b6be8288396
alexismierah <160734223+alexismierah@users.noreply.github.com>
2024-06-23 23:17:25 +0800

    Update PostDetail.jsx

--- frontend/src/PostDetail.jsx
+++ frontend/src/PostDetail.jsx
@@ -233,13 +233,13 @@ const PostDetail = () => {
  
     <button
       onClick={handleBack}
-      className="flex items-center mb-4 mt-2 ml-4 py-2 px-4 bg-white rounded-md text-slate-700 hover:bg-white"
+      className={`flex items-center mb-4 mt-20 ml-72 py-2 px-4 rounded-md ${theme === 'dark' ? 'bg-gray-900 text-white' : 'bg-white text-black'}`}
     >
       <FaArrowLeft className="mr-2 mt-4 text-gray-400 w-5 h-5" />
     </button>
       <button
         onClick={handleBack}
-        className="mb-4 mt-2 ml-4 py-2 px-4   text-white"
+        className="mb-0 mt-2 ml-4 py-2 px-4   text-white"
       >
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`size-6 fill-gray-700 ${theme === 'dark' ? 'fill-gray-500 hover:fill-gray-200 ' : 'fill-gray-500 hover:fill-gray-800'}`}>
           <path fillRule="evenodd" d="M11.03 3.97a.75.75 0 0 1 0 1.06l-6.22 6.22H21a.75.75 0 0 1 0 1.5H4.81l6.22 6.22a.75.75 0 1 1-1.06 1.06l-7.5-7.5a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" />
@@ -247,7 +247,7 @@ const PostDetail = () => {
 
       </button>
 
-      <div className={`flex-1 flex flex-col ${theme === 'dark' ? 'bg-gray-900 text-white' : 'bg-white text-black'}`}>
+      <div className={`flex-1 flex flex-col ml-80 mt-0 ${theme === 'dark' ? 'bg-gray-900 text-white' : 'bg-white text-black'}`}>
       <div className="flex flex-row  justify-center">
       <div className="col-span-2 space-y-4 w-[50rem]">
       <div className={` justify-center flex ${theme === 'dark' ? 'bg-gray-900 text-white' : 'bg-white text-black'}`}>
